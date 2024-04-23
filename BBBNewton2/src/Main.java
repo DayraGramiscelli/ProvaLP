63db08d4285985f65b541addc9a6de940595f4c6
DayraGramiscelli <131476671+DayraGramiscelli@users.noreply.github.com>
2024-04-23 19:32:19 -0300

    Add files via upload

---
+++ BBBNewton2/src/Main.java
@@ -0,0 +1,43 @@
+import javax.swing.*;
+import java.util.HashMap;
+import java.util.Map;
+import javax.swing.JOptionPane;
+
+public class Main {
+    public static void main(String[] args) {
+
+                HashMap<String, Integer> votos = new HashMap<>();
+
+                for (int i = 0; i < 5; i++) {
+                    String votoEm = JOptionPane.showInputDialog("Em quem você vota para sair da casa?");
+                    votos.put(votoEm, votos.getOrDefault(votoEm, 0) + 1);
+                }
+
+                StringBuilder votosText = new StringBuilder("Votos recebidos:\n");
+                for (Map.Entry<String, Integer> entry : votos.entrySet()) {
+                    votosText.append(entry.getKey()).append(": ").append(entry.getValue()).append("\n");
+                }
+                JOptionPane.showMessageDialog(null, votosText.toString(), "Votos Recebidos", JOptionPane.INFORMATION_MESSAGE);
+
+                String pessoaEliminada = "";
+                int maxVotos = 0;
+                boolean empate = false;
+                for (Map.Entry<String, Integer> entry : votos.entrySet()) {
+                    int votosParaJogador = entry.getValue();
+                    if (votosParaJogador > maxVotos) {
+                        maxVotos = votosParaJogador;
+                        pessoaEliminada = entry.getKey();
+                        empate = false;
+                    } else if (votosParaJogador == maxVotos) {
+                        empate = true;
+                    }
+                }
+
+                if (empate) {
+                    JOptionPane.showMessageDialog(null, "", "Empate", JOptionPane.INFORMATION_MESSAGE);
+                } else {
+
+                    JOptionPane.showMessageDialog(null, "O jogador eliminado é: " + pessoaEliminada + ".\n\nLamento, mas chegou a hora de deixar a casa. Obrigado por sua participação.", "Eliminação", JOptionPane.INFORMATION_MESSAGE);
+                }
+            }
+        }
